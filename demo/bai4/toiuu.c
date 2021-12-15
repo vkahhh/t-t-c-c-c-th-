cb2a8f39e7bf4f794b016a0e6a98b932394ff194
vkahhh <7.luhan.m.luexolu@gmail.com>
2021-12-14 21:34:58 -0800

    Them cac thu

---
+++ demo/bai4/toiuu.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int n = 100000;
+void KiemTraChanLe (int n) {
+     if (n % 2 == 0) printf ("n la so chan\n");
+     else printf ("n la so le");
+}
+int isPrimeNumber(int n) {
+    // so nguyen n < 2 khong phai la so nguyen to
+    if (n < 2) {
+        return 0;
+    }
+    // check so nguyen to khi n >= 2
+    int i;
+    int squareRoot = (int) sqrt(n);
+    for (i = 2; i <= squareRoot; i++) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+
+int main() {
+    int i;
+    KiemTraChanLe (n);
+    printf("Tat ca cac so nguyen to nho hon %d la: \n", n);
+    if (n >= 2) {
+        printf("%d ", 2);
+    }
+    for (i = 3; i < n; i+=2) {
+        if (isPrimeNumber(i) == 1) {
+            printf("%d ", i);
+        }
+    }
+    
+}
